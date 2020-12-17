b73883e3c3a5e5ba0e236b48c00c3efbc3c90a1b
Fil <filippkotenko@outlook.com>
2020-12-17 16:20:53 +0200

    final commit

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@ $(function(){
         speed: 500,
 });
 
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev "><img src="image/icons/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next "><img src="image/icons/arrow-right.svg" alt=""></button>',
+    });
+
 });
